625329c81311d84dc6fb6e0d897cfd7554b3ca58
angelator312 <112373005+angelator312@users.noreply.github.com>
2026-02-12 16:44:54 +0200

    added a way to have more quizzes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,27 @@
 import { Link } from "waku";
 import { Counter } from "../components/counter";
 import Quiz from "../components/Quiz";
-import Markdown from "react-markdown";
+import Markdown, { Components } from "react-markdown";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-import Example from "../quizes/ONotation.mdx";
+import OnotationQuiz from "../quizes/ONotation.mdx";
+import Quiz2 from "../quizes/Quiz2.mdx";
 import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
+import React, { JSX, ReactNode } from "react";
 
-/** @type {MDXComponents} */
-const components = {
-  Quiz,
-};
-
+const quizzes: { f: Function }[] = [
+  { f: OnotationQuiz },
+  { f: Quiz2 },
+];
 export default function HomePage() {
   // const data = await getData();
 
   return (
     <div>
-      <Example components={components} />
+      {quizzes.map((quiz) => {
+        const Component = quiz.f as React.FC;
+        return <Component components={{ Quiz }} />;
+      })}
       {/*<title>{data.title}</title>
       <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
       <p>{data.body}</p>*/}
